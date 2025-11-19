f9cc46bb39182dbacdeeabd99d5deddbabbee471
elecharu <elec_haru@naver.com>
2025-11-19 11:39:21 +0900

    ✅대시보드 UI 수정

--- src/components/dashboard/ui/DashboardSkeleton.tsx
+++ src/components/dashboard/ui/DashboardSkeleton.tsx
@@ -1,15 +1,15 @@
 export default function DashboardSkeleton() {
   return (
-    <div className="w-full h-full bg-gray-50 animate-pulse dark:bg-[#141c2c]">
+    <div className="w-full h-full bg-gray-50 animate-pulse dark:bg-[#182236]">
       <div className="w-full space-y-6">
         {/* 상단  */}
-        <div className="bg-white rounded-lg shadow-sm p-4 md:p-6 dark:bg-[#141c2c]">
+        <div className="bg-white rounded-lg shadow-sm p-4 md:p-6 border border-slate-300 dark:bg-[#141d2b] dark:border-[#364153]">
           <div className="h-14 bg-gray-100 rounded w-64 mb-16 dark:bg-[#1b263c]"></div>
           <div className="h-96 bg-gray-100 rounded-lg dark:bg-[#1b263c]"></div>
         </div>
 
         {/* 시장 요약 */}
-        <div className="bg-white rounded-lg shadow-sm p-4 md:p-6 min-h-40 flex items-center dark:bg-[#141c2c]">
+        <div className="bg-white rounded-lg shadow-sm p-4 md:p-6 min-h-40 flex items-center border border-slate-300 dark:bg-[#141d2b] dark:border-[#364153]">
           <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4 w-full">
             <div className="flex items-start gap-4">
               <div className="w-16 h-16 bg-gray-100 rounded-lg dark:bg-[#1b263c]"></div>
@@ -26,9 +26,9 @@ export default function DashboardSkeleton() {
         </div>
 
         {/* 3개 카드 영역 */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6 dark:bg-[#141c2c]">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6 border border-slate-300 dark:bg-[#141d2b] dark:border-[#364153]">
           {[1, 2, 3].map((i) => (
-            <div key={i} className="bg-white rounded-lg shadow-sm p-4 md:p-6 dark:bg-[#1b263c]">
+            <div key={i} className="bg-white rounded-lg shadow-sm p-4 md:p-6 dark:bg-[#141c2c]">
               <div className="h-6 bg-gray-100 rounded w-32 mb-4 dark:bg-[#1b263c]"></div>
               <div className="space-y-3">
                 {[1, 2, 3].map((j) => (
--- src/components/dashboard/ui/DashboardStats.tsx
+++ src/components/dashboard/ui/DashboardStats.tsx
@@ -129,24 +129,22 @@ export default function DashboardStats({ data }: DashboardStatsProps) {
   return (
     <>
       {/* 시장 요약 카드 */}
-      <div className="bg-white rounded-lg shadow-sm p-4 md:p-6 min-h-40 flex items-center dark:bg-[#141d2d]">
+      <div className="bg-white rounded-lg shadow-sm p-4 md:p-6 min-h-40 flex items-center border border-slate-300 dark:bg-[#141d2b] dark:border-[#364153]">
         <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4 w-full">
           <div className="flex items-start gap-4">
             <div className="bg-blue-100 p-3 rounded-lg dark:bg-[#1e2939]">
-              <Lightbulb className="w-10 h-10 text-blue-600 dark:text-indigo-500" />
+              <Lightbulb className="w-8 h-8 text-blue-600 dark:text-indigo-500" />
             </div>
             <div>
-              <h2 className="text-xl md:text-2xl font-bold text-gray-800 mb-1 dark:text-gray-300">
+              <h2 className="text-lg font-bold text-gray-800 mb-1 dark:text-gray-300">
                 오늘의 감정 시장지수 요약
               </h2>
-              <p className="text-gray-600 text-md md:text-lg dark:text-gray-400">{marketMessage}</p>
+              <p className="text-gray-600 text-md dark:text-gray-400">{marketMessage}</p>
             </div>
           </div>
           <div className="text-right">
-            <p className="text-sm sm:text-md md:text-lg text-gray-500 mb-1 dark:text-gray-300">
-              최근 업데이트
-            </p>
-            <div className="text-sm sm:text-md md:text-lg font-medium text-gray-700 dark:text-gray-400">
+            <p className="text-md text-gray-500 mb-1 dark:text-gray-300">최근 업데이트</p>
+            <div className="text-md font-medium text-gray-700 dark:text-gray-400">
               <p>{date}</p>
               {time && <p>{time}</p>}
             </div>
