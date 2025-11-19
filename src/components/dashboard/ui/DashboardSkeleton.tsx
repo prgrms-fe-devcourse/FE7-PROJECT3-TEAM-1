7fccfc4b9ea75fae940253325448d75e9b79122b
elecharu <elec_haru@naver.com>
2025-11-19 11:40:44 +0900

    ✅스켈레톤 ui 업데이트

--- src/components/dashboard/ui/DashboardSkeleton.tsx
+++ src/components/dashboard/ui/DashboardSkeleton.tsx
@@ -10,7 +10,7 @@ export default function DashboardSkeleton() {
 
         {/* 시장 요약 */}
         <div className="bg-white rounded-lg shadow-sm p-4 md:p-6 min-h-40 flex items-center border border-slate-300 dark:bg-[#141d2b] dark:border-[#364153]">
-          <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4 w-full">
+          <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4 w-full ">
             <div className="flex items-start gap-4">
               <div className="w-16 h-16 bg-gray-100 rounded-lg dark:bg-[#1b263c]"></div>
               <div className="flex-1">
@@ -26,7 +26,7 @@ export default function DashboardSkeleton() {
         </div>
 
         {/* 3개 카드 영역 */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6 border border-slate-300 dark:bg-[#141d2b] dark:border-[#364153]">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6 rounded-lg border border-slate-300 dark:bg-[#141d2b] dark:border-[#364153]">
           {[1, 2, 3].map((i) => (
             <div key={i} className="bg-white rounded-lg shadow-sm p-4 md:p-6 dark:bg-[#141c2c]">
               <div className="h-6 bg-gray-100 rounded w-32 mb-4 dark:bg-[#1b263c]"></div>
